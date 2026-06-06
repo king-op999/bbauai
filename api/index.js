144b84e333f77a096e695feefddc4400a718b554
king-op999 <ghosehisisi@gmail.com>
2026-06-06 11:28:06 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 
-const GROQ_API_KEY = process.env.GROQ_API_KEY || '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';
+const GROQ_API_KEY = process.env.GROQ_API_KEY || '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';
 const GROQ_URL = 'https://api.groq.com/openai/v1/responses';
 
 const MODELS = {
